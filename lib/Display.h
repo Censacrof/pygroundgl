e92d921aba8b5851604340d57f897ddd712f02ac
Censacrof <galisifrancesco@gmail.com>
2018-08-17 02:04:23 +0200

    Initial commit

---
+++ lib/Display.h
@@ -0,0 +1,38 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+
+
+class Display
+{    
+private:
+    SDL_Window* _mainWindow;
+    SDL_GLContext _context;
+
+    int _width;
+    int _height;
+    std::string _windowTitle;
+
+    bool _isWindowOpen;
+
+public:
+    Display(int, int, std::string);
+    ~Display();
+
+    void update();
+
+
+    int getWidth() { return _width; }
+    int getHeight() { return _height; }
+    float getRatio() { return (float) _width / (float) _height; }
+
+    bool isWindowsOpen() { return _isWindowOpen; }
+
+    void clear(float, float, float, float);
+};
+
+
+#endif
